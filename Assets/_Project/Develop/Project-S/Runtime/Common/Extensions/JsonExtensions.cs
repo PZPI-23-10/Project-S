865a538bdc189746a275d4b425bdcac7ec771648
Hlib Zoma <hlibzoma@gmail.com>
2026-03-16 19:35:59 +0100

    add more common extensions

---
+++ Assets/_Project/Develop/Project-S/Runtime/Common/Extensions/JsonExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Project_S.Runtime.Common.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson(this object obj) =>
+            JsonConvert.SerializeObject(obj);
+
+        public static T FromJson<T>(this string json, bool ignoreException = false)
+        {
+            if (!ignoreException)
+                return JsonConvert.DeserializeObject<T>(json);
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Project-S/Runtime/Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+
+namespace Project_S.Runtime.Common.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async UniTask<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            var timeoutCancellationSource = new CancellationTokenSource();
+
+            if (task == await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationSource.Token)))
+            {
+                timeoutCancellationSource.Cancel();
+                return await task;
+            }
+
+            throw new TimeoutException();
+        }
+    }
+}
